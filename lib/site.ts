c56d5b139d07ff96371a66a47241cf17ee97a72a
huangguojin <3130978830@qq.com>
2026-05-24 14:27:23 +0800

    chore: update site base URL to devtimekit.xyz

--- lib/site.ts
+++ lib/site.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://devtimekit.com';
+export const BASE_URL = 'https://devtimekit.xyz';
 export const SITE_NAME = 'DevTimeKit';
 export const SITE_DESCRIPTION =
   'Convert Unix timestamps instantly. View epoch time in seconds, milliseconds, UTC, ISO 8601 and local time.';
